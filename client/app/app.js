6393c26a0f30335019760e499aeac88146cb507e
UnJavaScripter <eltu.khan@gmail.com>
2016-04-23 12:32:31 -0500

    Add a map

--- client/app/app.js
+++ client/app/app.js
@@ -10,11 +10,18 @@ angular.module('fishackatonApp', [
   'btford.socket-io',
   'ui.router',
   'ui.bootstrap',
-  'validation.match'
+  'validation.match',
+  'uiGmapgoogle-maps'
 ])
-  .config(function($urlRouterProvider, $locationProvider) {
+  .config(function($urlRouterProvider, $locationProvider, uiGmapGoogleMapApiProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
+
+    uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
   });
--- client/app/plantas/plantas.controller.js
+++ client/app/plantas/plantas.controller.js
@@ -3,20 +3,30 @@
 
 class PlantasComponent {
 
-  constructor($http, $scope, socket) {
+  constructor($http, $scope, socket, uiGmapGoogleMapApi) {
     this.$http = $http;
     this.socket = socket;
     this.plantas = [];
 
-
-
     $scope.$on('$destroy', function() {
       socket.unsyncUpdates('planta');
     });
+
   }
 
   $onInit() {
     this.$http.get('/api/plantas').then(response => {
+      response.data.map((onePlant) => {
+        onePlant.location.map((location, index, arr) =>{
+          location.id = index+1;
+          location.latitude = location.lat;
+          location.longitude = location.lng;
+
+          delete location.lat;
+          delete location.lng;
+        })
+      });
+
       this.plantas = response.data;
       this.socket.syncUpdates('planta', this.plantas);
     });
@@ -38,7 +48,7 @@ class PlantasComponent {
     this.newPlanta = {};
     this.newPlanta.colloquial_names = [];
     this.newPlanta.pictures = [];
-    this.newPlanta.location = [{lat: 4, lng: -74}];
+    this.newPlanta.location = [];
   }
 
 }
